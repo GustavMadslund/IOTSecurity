64dddf8b6b771c47568257c853559d33650210b1
GustavMadslund <Gustav.Madslund@gmail.com>
2017-04-20 12:26:52 +0200

    Added fields and method in Connection

--- src/Connection.java
+++ src/Connection.java
@@ -1,8 +1,34 @@
-/**
- * Created by GustavMadslund on 20/04/2017.
- */
+
 public class Connection {
     private Device firstDevice;
     private Device secondDevice;
+    private Formats format;
+    private ConnectionType connectionType;
+
+    public Connection(Device firstDevice, Device secondDevice, Formats format, ConnectionType connectionType) {
+        this.firstDevice = firstDevice;
+        this.secondDevice = secondDevice;
+        this.format = format;
+        this.connectionType = connectionType;
+    }
+
+    public Device getFirstDevice() {
+        return firstDevice;
+    }
+
+    public Device getSecondDevice() {
+        return secondDevice;
+    }
+
+    public Formats getFormat() {
+        return format;
+    }
+
+    public ConnectionType getConnectionType() {
+        return connectionType;
+    }
+
+
+
 
 }
---
+++ src/ConnectionType.java
@@ -0,0 +1,4 @@
+
+public enum ConnectionType {
+    ZIGBEE, WIFI, BLUETOOTH
+}
