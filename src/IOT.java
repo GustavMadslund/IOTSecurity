22a99a1ddbb6b8bd7f28dd2f5e44e092891d125c
SpacePlant <SpacePlant@users.noreply.github.com>
2017-04-20 12:06:00 +0200

    parser test

--- src/IOT.java
+++ src/IOT.java
@@ -1,5 +1,16 @@
-/**
- * Created by GustavMadslund on 20/04/2017.
- */
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+
 public class IOT {
+    public static void main(String[] args) throws Exception{
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = db.parse(new File("xml/case1.xml"));
+
+        System.out.println(doc.getElementsByTagName("device").item(0).getAttributes().getNamedItem("name"));
+    }
 }
