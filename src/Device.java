b72c5713cd127cac26fe6dfa1ff096c9c5905d58
GustavMadslund <Gustav.Madslund@gmail.com>
2017-04-20 12:55:06 +0200

    Added fields and methods to Device

--- src/Device.java
+++ src/Device.java
@@ -1,10 +1,38 @@
 import Enums.DeviceType;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Device {
     private String name;
     private DeviceType deviceType;
     private List<Dimension> dimensions;
+    private List<Connection> connections;
 
+    public Device(String name, DeviceType deviceType, List<Dimension> dimensions) {
+        this.name = name;
+        this.deviceType = deviceType;
+        this.dimensions = dimensions;
+        connections = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public DeviceType getDeviceType() {
+        return deviceType;
+    }
+
+    public List<Dimension> getDimensions() {
+        return dimensions;
+    }
+
+    public List<Connection> getConnections() {
+        return connections;
+    }
+
+    public void addConnection(Connection connection){
+        connections.add(connection);
+    }
 }
